0136292842798ea78e1a17755526adc5650e6c31
matheusccastro <camargodecastromatheus@gmail.com>
2024-02-28 14:18:40 -0300

    feat: create main screen to trigger SDK on sample

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-csliveness-react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [clientId, setClientId] = React.useState<string | undefined>();
+  const [clientSecret, setClientSecret] = React.useState<string | undefined>();
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        value={clientId}
+        placeholder="ClientID"
+        onChangeText={setClientId}
+      />
+      <TextInput
+        style={styles.input}
+        value={clientSecret}
+        placeholder="Client Secret"
+        onChangeText={setClientSecret}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonTitle}>Open CSLiveness</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,10 +39,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  input: {
+    width: '90%',
+    borderWidth: 5,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    width: '90%',
+  },
+  buttonTitle: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
