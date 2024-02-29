6bfa477afc8b35c8c7d74a54c1cb9ca23040d932
matheusccastro <camargodecastromatheus@gmail.com>
2024-02-28 22:01:09 -0300

    feat: create android plugin

--- android/src/main/java/com/cslivenessreactnative/CslivenessReactNativeModule.kt
+++ android/src/main/java/com/cslivenessreactnative/CslivenessReactNativeModule.kt
@@ -1,22 +1,98 @@
 package com.cslivenessreactnative
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import com.clear.studio.csliveness.core.CSLiveness
+import com.clear.studio.csliveness.core.CSLivenessResult
+import com.clear.studio.csliveness.view.CSLivenessActivity
+import com.facebook.react.bridge.ActivityEventListener
+import com.facebook.react.bridge.BaseActivityEventListener
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeMap
+
 
 class CslivenessReactNativeModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+
+  private val requestCode: Int = 40
+  private var promise: Promise? = null
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun openCSLiveness(sdkParams: ReadableMap, promise: Promise) {
+    val clientId: String = if (sdkParams.hasKey("clientId") && sdkParams.getString("clientId") != null) sdkParams.getString("clientId")!! else throw Exception("clientId is required")
+    val clientSecretId: String = if (sdkParams.hasKey("clientSecretId") && sdkParams.getString("clientSecretId") != null) sdkParams.getString("clientSecretId")!! else throw Exception("clientSecretId is required")
+    val vocalGuidance: Boolean = if (sdkParams.hasKey("vocalGuidance")) sdkParams.getBoolean("vocalGuidance") else false
+    val identifierId: String? = if (sdkParams.hasKey("identifierId") && sdkParams.getString("identifierId") != null) sdkParams.getString("identifierId") else null
+    val cpf: String? = if (sdkParams.hasKey("cpf") && sdkParams.getString("cpf") != null) sdkParams.getString("cpf") else null
+
+    this.promise = promise
+
+    try {
+      val csLiveness = CSLiveness(clientId, clientSecretId, vocalGuidance, identifierId, cpf)
+
+      val intent = Intent(reactApplicationContext, CSLivenessActivity::class.java)
+      intent.putExtra(CSLiveness.PARAMETER_NAME, csLiveness)
+
+      reactApplicationContext.startActivityForResult(intent, requestCode, null)
+    } catch (t: Throwable) {
+      Log.e("[CSLiveness]", "Error starting CSLivenessSDK", t)
+      this.promise?.reject("Failed to start CSLivenessSDK", t)
+      this.promise = null
+    }
+  }
+
+  private val activityEventListener: ActivityEventListener = object : BaseActivityEventListener() {
+    override fun onActivityResult(
+      activity: Activity,
+      activityRequestCode: Int,
+      activityResultCode: Int,
+      data: Intent?
+    ) {
+      if (activityRequestCode == requestCode) {
+        val responseMap: WritableMap = WritableNativeMap()
+
+        if (activityResultCode == Activity.RESULT_OK && data != null) {
+          val csLivenessResult =
+            data.getSerializableExtra(CSLiveness.PARAMETER_NAME) as CSLivenessResult
+
+          val responseMessage = csLivenessResult.responseMessage;
+          responseMap.putString("responseMessage", responseMessage)
+
+          val sessionId = csLivenessResult.sessionId
+          responseMap.putString("sessionId", sessionId)
+
+          val image = csLivenessResult.image
+          responseMap.putString("image", image)
+
+          Log.d("[CSLiveness] Result", responseMap.toString())
+
+          promise?.resolve(responseMap)
+        } else {
+          responseMap.putString("responseMessage", "UserCancel")
+          Log.d("[CSLiveness] Result", responseMap.toString())
+
+          promise?.resolve(responseMap)
+        }
+
+        promise = null
+      }
+
+      super.onActivityResult(activity, activityRequestCode, activityResultCode, data)
+    }
+  }
+
+  init {
+    reactContext.addActivityEventListener(activityEventListener)
   }
 
   companion object {
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, PermissionsAndroid, Platform } from 'react-native';
+import SimpleSchema from 'simpl-schema';
 
 const LINKING_ERROR =
   `The package 'react-native-csliveness-react-native' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +18,85 @@ const CslivenessReactNative = NativeModules.CslivenessReactNative
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return CslivenessReactNative.multiply(a, b);
-}
+const CSLivenessSchema = new SimpleSchema({
+  clientId: String,
+  clientSecretId: String,
+  vocalGuidance: {
+    type: Boolean,
+    optional: true,
+    defaultValue: false,
+  },
+  identifierId: {
+    type: String,
+    defaultValue: null,
+    optional: true,
+  },
+  cpf: {
+    type: String,
+    defaultValue: null,
+    optional: true,
+  },
+});
+
+type CSLivenessConfiguration = {
+  clientId: string;
+  clientSecretId: string;
+  vocalGuidance?: boolean;
+  identifierId?: string | null;
+  cpf?: string | null;
+};
+
+type CSLivenessResult = {
+  responseMessage: string;
+  sessionId: string | null;
+  image: string | null;
+};
+
+const isAndroid = Platform.OS === 'android';
+const maybeRequireAndroidCameraPermissions = async (
+  androidNeverAskPermissionAgainCallback?: () => {}
+): Promise<void> => {
+  if (!isAndroid) {
+    return;
+  }
+
+  if (
+    !(await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA!))
+  ) {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA!
+    );
+
+    if (granted === PermissionsAndroid.RESULTS.DENIED) {
+      throw new Error('PermissionNotGrantedError');
+    }
+
+    if (granted === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      if (androidNeverAskPermissionAgainCallback) {
+        androidNeverAskPermissionAgainCallback();
+      }
+    }
+  }
+};
+
+export const useCSLiveness = () => {
+  return {
+    open: async (
+      csLivenessConfiguration: CSLivenessConfiguration,
+      androidNeverAskPermissionAgainCallback?: () => {}
+    ): Promise<CSLivenessResult> => {
+      const cleanedDoc = CSLivenessSchema.clean(csLivenessConfiguration, {
+        getAutoValues: true,
+        trimStrings: true,
+      });
+
+      CSLivenessSchema.validate(cleanedDoc);
+
+      await maybeRequireAndroidCameraPermissions(
+        androidNeverAskPermissionAgainCallback
+      );
+
+      return CslivenessReactNative.openCSLiveness(cleanedDoc);
+    },
+  };
+};
